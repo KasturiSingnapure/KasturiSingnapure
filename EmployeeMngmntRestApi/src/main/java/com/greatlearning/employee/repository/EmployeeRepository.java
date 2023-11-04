9f42007c0f4b2275548fef4963d50d2f0eb0ad6c
KasturiSingnapure <134558446+KasturiSingnapure@users.noreply.github.com>
2023-11-04 18:09:52 +0530

    Delete EmployeeMngmntRestApi directory

--- EmployeeMngmntRestApi/src/main/java/com/greatlearning/employee/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.employee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greatlearning.employee.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-	// You can define custom query methods here if needed
-}
